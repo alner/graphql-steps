3ffe512fdd93e83285094a3e06236a661ab88ab2
alex.nerush <ale.nerush@gmail.com>
2018-11-25 14:30:52 +0200

    GraphQLEnumType

--- src/index.js
+++ src/index.js
@@ -2,36 +2,67 @@
   Based on:
   https://github.com/graphql/graphql-js
 
+  See more examples:
+  https://github.com/graphql/graphql-js/blob/master/src/__tests__/starWarsSchema.js
+
 */
 
 const {
   graphql,
   GraphQLSchema,
   GraphQLObjectType,
-  GraphQLString
+  GraphQLNonNull,
+  GraphQLString,
+  GraphQLEnumType
 } = require("graphql");
 
-/*
-const schema = buildSchema(`
-type Query {
-  info: String
-}`);
-*/
+const KindEnum = new GraphQLEnumType({
+  name: "Kind",
+  description: "Info kind enum",
+  values: {
+    REQUEST: {
+      value: 1,
+      description: "Info request"
+    },
+    RESPONSE: {
+      value: 2,
+      description: "Response info response"
+    }
+  }
+});
+
+const InfoType = new GraphQLObjectType({
+  name: "Info",
+  fields: () => ({
+    kind: {
+      type: KindEnum,
+      description: "Info kind"
+    },
+    description: {
+      type: GraphQLNonNull(GraphQLString),
+      description: "Info description"
+    }
+  })
+});
+
 const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: "Query",
     fields: {
       info: {
-        type: GraphQLString,
+        type: InfoType,
         args: {
           kind: {
-            type: GraphQLString,
+            type: KindEnum,
             description: "info kind parameter"
           }
         },
         resolve(root, args) {
           console.log("Args: ", args);
-          return `Info ${args.kind || "graphql"}!`;
+          return {
+            kind: args.kind,
+            description: `Hello ${args.kind || "graphql"}!`
+          };
         }
       }
     }
@@ -39,7 +70,10 @@ const schema = new GraphQLSchema({
 });
 
 const query = `{
-  info(kind: "test")
+  info(kind: REQUEST) {
+    kind
+    description
+  }
 }`;
 
 graphql(schema, query).then(console.log);
